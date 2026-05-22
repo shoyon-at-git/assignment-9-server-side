7745dd5fe2a5e2bbdc3992e07a1468a93c527851
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-05-22 20:13:27 +0600

    Added run function and successfully connected to mongodb

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const cors = require("cors")
 const app=express();
 const port = process.env.PORT;
 
+app.use(cors());
+app.use(express.json());
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -13,6 +16,19 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req,res)=>{
     res.send("server is running properly,,");
