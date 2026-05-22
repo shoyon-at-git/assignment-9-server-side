482927924898e540055dda11c0f6766629daac77
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-05-22 19:52:17 +0600

    Added mongoClient for connection

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
 const cors = require("cors")
 const app=express();
-
 const port = process.env.PORT;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/', (req,res)=>{
     res.send("server is running properly,,");
