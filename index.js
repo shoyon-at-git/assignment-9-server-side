9ec401b8f4e1db25f4ca1b835a8f582a531cda76
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-05-22 19:36:45 +0600

    added get api for /

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const cors = require("cors")
 const app=express();
 
 const port = process.env.PORT;
-console.log(port);
+
+app.get('/', (req,res)=>{
+    res.send("server is running properly,,");
+})
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
