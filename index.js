da46f541e5305af78e07a8a4558026f143c6fa92
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-05-27 14:14:09 +0600

    Added client url from env

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 });
 
 const JWKS = createRemoteJWKSet(
-  new URL("http://localhost:3000/api/auth/jwks")
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
 )
 const verifyToken = async(req,res,next)=>{
   const authHeader = req?.headers.authorization;
@@ -42,9 +42,9 @@ const verifyToken = async(req,res,next)=>{
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
